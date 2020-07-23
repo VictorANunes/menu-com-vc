03ff2c96e4ac3839f646bfd244495a33893c4355
VictorANunes <68710291+VictorANunes@users.noreply.github.com>
2020-07-23 17:13:10 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,59 @@
+function clarear(){
+    let vinho = document.querySelector(".img-vinho");
+    vinho.style.opacity = "0.8"
+}
+
+function normalizar(){
+    let vinho = document.querySelector(".img-vinho");
+    vinho.style.opacity = "1"
+}
+
+function clarear1(){
+    let vinho1 = document.querySelector(".img-vinho-branco");
+    vinho1.style.opacity = "0.8"
+}
+
+function normalizar1(){
+    let vinho1 = document.querySelector(".img-vinho-branco");
+    vinho1.style.opacity = "1"
+}
+
+function clarear2(){
+    let vinho1 = document.querySelector(".img-espumante");
+    vinho1.style.opacity = "0.8"
+}
+
+function normalizar2(){
+    let vinho1 = document.querySelector(".espumante");
+    vinho1.style.opacity = "1"
+}
+
+function clarear3(){
+    let prod = document.querySelector(".prod");
+    prod.style.opacity = "0.8"
+}
+
+function normalizar3(){
+    let prod = document.querySelector(".prod");
+    prod.style.opacity = "1"
+}
+
+function clarear4(){
+    let prod1 = document.querySelector(".prod1");
+    prod1.style.opacity = "0.8"
+}
+
+function normalizar4(){
+    let prod1 = document.querySelector(".prod1");
+    prod1.style.opacity = "1"
+}
+
+function clarear5(){
+    let prod2 = document.querySelector(".prod2");
+    prod2.style.opacity = "0.8"
+}
+
+function normalizar5(){
+    let prod2 = document.querySelector(".prod2");
+    prod2.style.opacity = "2"
+}
